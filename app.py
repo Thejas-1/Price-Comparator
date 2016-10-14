fcf72a7450ba9d599d914f485e0646809f959b12
Thejas-1 <naiktheju@gmail.com>
2016-10-15 00:38:49 +0530

    Price Comparator

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
+
+print(os.environ['APP_SETTINGS'])
